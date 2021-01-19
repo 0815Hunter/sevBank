31b30ecd42d12bebb1d5850bb202901aa33f4841
0815Hunter <sebastianfey96@gmx.de>
2021-01-19 01:09:23 +0100

    TermAccountMapper: annotations reordered

--- src/main/java/de/sevdesk/api/account/data/mapper/TermAccountMapper.java
+++ src/main/java/de/sevdesk/api/account/data/mapper/TermAccountMapper.java
@@ -10,10 +10,10 @@ import org.mapstruct.Mapping;
 @Mapper(config = QuarkusMapperConfig.class)
 public interface TermAccountMapper {
 
-    @Mapping(target = "customerIds", ignore = true)
     @Mapping(target = "withdrawalBlockedUntilDate", ignore = true)
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "balance", ignore = true)
+    @Mapping(target = "customerIds", ignore = true)
     @Mapping(target = "associatedCustomers", ignore = true)
     TermAccount toTermAccount(TermAccountPost termAccountPost);
 
